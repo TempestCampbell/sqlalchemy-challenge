da9f9e5eea102c1e53e1dbc117697c1dbd129eae
Tempest <tempest@MacDeth.hsd1.co.comcast.net>
2021-03-20 00:42:36 -0600

    begin flask routes

--- app.py
+++ app.py
@@ -24,3 +24,18 @@ Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
+
+# Flask
+app = Flask(__name__)
+
+# Flask Routes
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )

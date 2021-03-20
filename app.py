c95fbe91c2049bfbb82b5579a69ecdc6f9c6a7a8
Tempest <tempest@MacDeth.hsd1.co.comcast.net>
2021-03-20 01:06:07 -0600

    flask routes complete

--- app.py
+++ app.py
@@ -39,12 +39,59 @@ def welcome():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/temp/start/end"
     )
+
 @app.route("/api/v1.0/precipitation")
+def precipitation():
+dateYRago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    dataPrecip = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+
+    precip = {date: prcp for date, prcp in dataPrecip}
+    return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
 
 @app.route("/api/v1.0/tobs")
+def mo_of_temps():
+ """Return the temperature observations (tobs) for previous year."""
+    # Calculate the date 1 year ago from last date in database
+    dateYRago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the primary station for all tobs from the last year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= dateYRago).all()
+
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+
+    # Return the results
+    return jsonify(temps=temps)
+
 
 @app.route("/api/v1.0/temp/<start>")
-
 @app.route("/api/v1.0/temp/<start>/<end>")
+def info(start=None, end=None):
+ sel = [func.min(Measurement.tobs), 
+  func.avg(Measurement.tobs), 
+  func.max(Measurement.tobs)]
+
+if not end:
+ results = session.query(*sel).\
+     filter(Measurement.date >= start).all()
+ temps = list(np.ravel(results))
+ return jsonify(temps)
+
+ results = session.query(*sel).\
+     filter(Measurement.date >= start).\
+     filter(Measurement.date <= end).all()
+ temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+if __name__ == '__main__':
+    app.run()
